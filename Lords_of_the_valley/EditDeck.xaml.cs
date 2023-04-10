890da84c945a31a7c7efe9ff41f5d625af71386e
roiqd03 <roiquint@ucm.es>
2023-04-10 22:29:32 +0200

    cartas hechas

--- Lords_of_the_valley/EditDeck.xaml.cs
+++ Lords_of_the_valley/EditDeck.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -12,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using static System.Net.Mime.MediaTypeNames;
 
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,15 +24,58 @@ namespace Lords_of_the_valley
     /// </summary>
     public sealed partial class EditDeck : Page
     {
-        public List<DeckModel> Decks { get; set; } = new List<DeckModel>();
+        private double startWidth = 1920;
+        private double startHeight = 1080;
+        public List<DeckModel> CardPlace { get; set; } = new List<DeckModel>();
+        public List<CardModel> Cards { get; set; } = new List<CardModel>();
         public EditDeck()
         {
             this.InitializeComponent();
 
-            for(int i = 0; i < 30; ++i)
+            //this.SizeChanged += Window_SizeChanged;
+
+            for (int i = 0; i < 30; ++i)
             {
-                Decks.Add(new DeckModel("a", "Card Name " + i, i));
+                CardPlace.Add(new DeckModel("a", "Card Name " + i, i));
             }
+
+            for (int i = 0; i < 30; ++i)
+            {
+                Cards.Add(new CardModel("Assets\\DecksImg\\deck1.jpg", "Card Description " + i, 3, 1, 2, i));
+            }
+        }
+
+        //private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        //{
+        //    // calculates incorrect when window is maximized
+        //    double scale = (this.ActualWidth * this.ActualHeight / startWidth / startHeight);
+        //    double w = cardWidth * scale;
+        //    double h = cardHeight * scale;
+
+        //    foreach (CardModel c in Cards)
+        //    {
+        //        c.width = w;
+        //        c.height = h;
+        //    }
+        //}
+    }
+
+    public class CardModel
+    {
+        public string imgSource;
+        public string description;
+        public int attack;
+        public int armor;
+        public int mana;
+        public int id;
+        public CardModel(string img, string desc, int attack_, int armor_, int mana_, int id_)
+        {
+            this.imgSource = img;
+            this.description = desc;
+            this.attack = attack_;
+            this.armor = armor_;
+            this.mana = mana_;
+            this.id = id_;
         }
     }
 }
